a8f9450087ef54480ade355c99b84d7aacca81aa
Nagaraj Narayana <nagraj.24ece@cambridge.edu.in>
2026-05-26 14:55:04 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,73 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
+    dut._log.info("Start Comparator Test")
+
+    # Create clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    await ClockCycles(dut.clk, 5)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # =========================================
+    # Test 1 : Equal Condition
+    # =========================================
+    dut._log.info("Test 1 : Equal")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 10
+    dut.uio_in.value = 10
 
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 1, \
+        f"FAILED: Expected 1, Got {dut.uo_out.value}"
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # =========================================
+    # Test 2 : Greater Than
+    # =========================================
+    dut._log.info("Test 2 : Greater Than")
+
+    dut.ui_in.value = 25
+    dut.uio_in.value = 10
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 2, \
+        f"FAILED: Expected 2, Got {dut.uo_out.value}"
+
+    # =========================================
+    # Test 3 : Less Than
+    # =========================================
+    dut._log.info("Test 3 : Less Than")
+
+    dut.ui_in.value = 5
+    dut.uio_in.value = 20
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 4, \
+        f"FAILED: Expected 4, Got {dut.uo_out.value}"
+
+    # =========================================
+    # Test 4 : Equal
+    # =========================================
+    dut._log.info("Test 4 : Equal")
+
+    dut.ui_in.value = 100
+    dut.uio_in.value = 100
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 1, \
+        f"FAILED: Expected 1, Got {dut.uo_out.value}"
+
+    dut._log.info("All tests passed!")
